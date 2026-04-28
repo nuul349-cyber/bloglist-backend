a45d04818c69e58dfe360f97427ac37f173eb90c
Nuul <nuul@example.com>
2026-04-28 17:39:50 -0600

    refactor app into app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const { unknownEndpoint } = require('./utils/middleware')
+
+
+const app = express()
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error conecting to MongoDB: ', error.message)
+  })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(unknownEndpoint)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const express = require('express')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
-const { unknownEndpoint } = require('./utils/middleware')
+const app = require('./app')
+const { PORT } = require('./utils/config')
+const { info } = require('./utils/logger')
 
 
-const app = express()
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { family: 4 })
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch(error => {
-    logger.error('error conecting to MongoDB: ', error.message)
-  })
-
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(unknownEndpoint)
-
-const PORT = config.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  info(`Server running on port ${PORT}`)
 })
