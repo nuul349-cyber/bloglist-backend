33f4955e2b7748e59b0f089cbcfd1d6a66f506e7
Nuul <nuul@example.com>
2026-04-28 18:45:26 -0600

    added totalLikes to list_helper.js and its test

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -8,3 +8,79 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listManyBlogs = [
+    {
+      _id: '5a422aa71b54f676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a67623sd17f8',
+      title: 'Go To Statement Harmful',
+      author: 'EdW. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Ditra68.pdf',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54ah76234d17f8',
+      title: 'Go Statement Considered Harmful',
+      author: 'W. Dtra',
+      url: 'https://homepages.cwi.nl/~storm/teacing/reader/Dijksra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234h17f8',
+      title: 'Go To Statement Considered',
+      author: 'W. Dijkstra',
+      url: 'https://homepages.cwi.nl/orm/teaching/reader/Dijkstra68.pdf',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa7hb54a676234d17f8',
+      title: 'To Statement Considered Harmful',
+      author: 'Edsger W.',
+      url: 'https://homepages.cwi.nltorm/teaching/reader/Dijkstra68.pdf',
+      likes: 2,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54ah76234d17f8',
+      title: 'Go Considered Harmful',
+      author: 'Edsger Dijkstra',
+      url: 'https://homepagewi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 9,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 5)
+  })
+
+  test('of a bigger list is calculated adding all likes', () => {
+    assert.strictEqual(listHelper.totalLikes(listManyBlogs), 31)
+  })
+})
